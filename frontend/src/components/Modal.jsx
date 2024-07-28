016d8fb42906cb8dc584fc81e26dc2bb5e7ba1d1
Augustofrx <augustoiphar99@hotmail.com>
2024-07-28 16:17:04 -0300

    feat: progress bar when delete all products with socket connection

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -5,10 +5,7 @@ const Modal = ({ isOpen, onClose, children }) => {
     <>
       {isOpen && (
         <div className="fixed inset-0 flex items-center justify-center z-50">
-          <div
-            className="fixed inset-0 bg-black opacity-50"
-            onClick={onClose}
-          ></div>
+          <div className="fixed inset-0 bg-black opacity-50"></div>
           <div className="relative min-w-[500px] bg-white p-6 rounded-lg z-50 max-w-3xl mx-auto">
             <button
               className="btn btn-sm btn-error text-white btn-circle absolute right-2 top-2"
--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useQuery } from "react-query";
 import axios from "../axios.config";
 import Modal from "./Modal";
@@ -6,6 +6,7 @@ import EditProductForm from "./EditProductForm";
 import Swal from "sweetalert2";
 import Navbar from "./Navbar";
 import DeleteConfirmationForm from "./DeleteConfirmationForm";
+import io from "socket.io-client";
 
 const fetchProducts = async (isDeleted) => {
   const response = await axios.get(`/products?isDeleted=${isDeleted}`);
@@ -27,6 +28,26 @@ const Products = () => {
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
   const [isDeleteAllModalOpen, setIsDeleteAllModalOpen] = useState(false);
   const [productIdToDelete, setProductIdToDelete] = useState(null);
+  const [deleteProgress, setDeleteProgress] = useState(0);
+  const [progressVisible, setProgressVisible] = useState(false);
+
+  useEffect(() => {
+    const socket = io("http://localhost:3001");
+    socket.on("deleteProgress", ({ progress }) => {
+      setDeleteProgress(progress);
+      setProgressVisible(true);
+
+      if (progress === 100) {
+        setTimeout(() => {
+          setProgressVisible(false);
+        }, 500);
+      }
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
 
   const handleEdit = (product) => {
     setSelectedProduct(product);
@@ -38,13 +59,13 @@ const Products = () => {
   };
 
   const handleDeleteAll = async () => {
-    await axios.put(`/products/deleteAll`);
-    refetch();
-    await Swal.fire({
-      icon: "success",
-      title: "Productos eliminados",
-      text: "Los productos se han eliminado correctamente.",
-    });
+    try {
+      setIsDeleteAllModalOpen(false);
+      await axios.put(`/products/deleteAll`);
+      refetch();
+    } catch (error) {
+      console.error("Error deleting all products:", error);
+    }
   };
 
   const openDeleteAllConfirmModal = () => {
@@ -111,23 +132,31 @@ const Products = () => {
     <div className="max-w-6xl mx-auto">
       <Navbar />
       <div className="max-w-6xl mx-auto border p-4 rounded-lg shadow-md my-4 bg-gray-50">
-        <section className="flex flex-col md:flex-row justify-between items-center md:items-start mb-4 md:mb-0">
-          <h2 className="text-2xl  mb-4">Lista de Smartphones</h2>
-          <div className="flex gap-2">
-            <button
-              disabled={sortedProducts.length === 0}
-              onClick={openDeleteAllConfirmModal}
-              className="btn btn-sm btn-error text-white"
-            >
-              Eliminar todos
-            </button>
-            <button
-              onClick={handleLoadTests}
-              className="btn btn-sm btn-warning "
-            >
-              Cargar productos de prueba
-            </button>
+        <section className="flex flex-col justify-between items-center md:items-start mb-4 md:mb-0">
+          <div className="flex justify-between items-center md:items-start mb-4 md:mb-0 w-full">
+            <h2 className="text-2xl mb-4">Lista de Smartphones</h2>
+
+            <div className="flex gap-2">
+              <button
+                disabled={sortedProducts.length === 0}
+                onClick={openDeleteAllConfirmModal}
+                className="btn btn-sm btn-error text-white"
+              >
+                Eliminar todos
+              </button>
+              <button
+                onClick={handleLoadTests}
+                className="btn btn-sm btn-warning"
+              >
+                Cargar productos de prueba
+              </button>
+            </div>
           </div>
+          {progressVisible && deleteProgress > 0 && (
+            <div className="mb-2 rounded-md w-full bg-blue-500 text-white text-center">
+              Progreso de eliminación: {deleteProgress}%
+            </div>
+          )}
         </section>
         <div className="mb-4">
           <input
@@ -159,7 +188,6 @@ const Products = () => {
                   <th>Modelo</th>
                   <th>Marca</th>
                   <th>Descripción</th>
-
                   <th>
                     <span>Precio</span>
                     <button
@@ -170,7 +198,6 @@ const Products = () => {
                       {sortOrder === "asc" ? "⬆️" : "⬇️"}
                     </button>
                   </th>
-
                   <th></th>
                 </tr>
               </thead>
@@ -181,7 +208,6 @@ const Products = () => {
                     <td>{product.brand}</td>
                     <td>{product.description}</td>
                     <td>${product.price}</td>
-
                     <td className="flex gap-2">
                       <button
                         className="btn btn-sm btn-primary mr-2"
@@ -201,42 +227,40 @@ const Products = () => {
               </tbody>
             </table>
           ) : (
-            <div className="w-full h-full relative flex justify-center items-center">
-              <span className="text-error font-semibold">
-                No se han encontrado productos
-              </span>
+            <div className="w-full h-full flex justify-center items-center text-center">
+              <span>No se encontraron productos.</span>
             </div>
           )}
         </div>
-
-        <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
-          {selectedProduct && (
-            <EditProductForm
-              product={selectedProduct}
-              onClose={() => setIsModalOpen(false)}
-              onSave={handleSave}
-            />
-          )}
-        </Modal>
-
-        <DeleteConfirmationForm
-          isOpen={isDeleteModalOpen}
-          onClose={() => setIsDeleteModalOpen(false)}
-          onConfirm={confirmDelete}
-          context={
-            "¿Estás seguro de que deseas eliminar este producto? Esta acción es irreversible."
-          }
-        />
-
-        <DeleteConfirmationForm
-          isOpen={isDeleteAllModalOpen}
-          onClose={() => setIsDeleteAllModalOpen(false)}
-          onConfirm={handleDeleteAll}
-          context={
-            "¿Estás seguro de que deseas eliminar todos los productos? Esta acción es irreversible."
-          }
-        />
       </div>
+
+      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+        {selectedProduct && (
+          <EditProductForm
+            product={selectedProduct}
+            onClose={() => setIsModalOpen(false)}
+            onSave={handleSave}
+          />
+        )}
+      </Modal>
+
+      <DeleteConfirmationForm
+        isOpen={isDeleteModalOpen}
+        onClose={() => setIsDeleteModalOpen(false)}
+        onConfirm={confirmDelete}
+        context={
+          "¿Estás seguro de que deseas eliminar este producto? Esta acción es irreversible."
+        }
+      />
+
+      <DeleteConfirmationForm
+        isOpen={isDeleteAllModalOpen}
+        onClose={() => setIsDeleteAllModalOpen(false)}
+        onConfirm={handleDeleteAll}
+        context={
+          "¿Estás seguro de que deseas eliminar todos los productos? Esta acción es irreversible."
+        }
+      />
     </div>
   );
 };
