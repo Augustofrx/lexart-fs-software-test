2c1af0787be4afa023dc6af644746d7fd38fe820
Augustofrx <augustoiphar99@hotmail.com>
2024-07-28 19:32:13 -0300

    fix: cors errors

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "https://lexart-fs-software-test-kcbo.vercel.app/",
+    origin: "https://lexart-fs-software-test-kcbo.vercel.app",
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type"],
   },
@@ -35,6 +35,7 @@ app.use(
     origin: "https://lexart-fs-software-test-kcbo.vercel.app",
   })
 );
+
 app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
