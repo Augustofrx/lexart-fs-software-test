a616b2dc590d5c51972841b32906c21b5c49cc6a
Augustofrx <augustoiphar99@hotmail.com>
2024-07-28 21:01:20 -0300

    change: backend url for socket connection

--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -32,7 +32,7 @@ const Products = () => {
   const [progressVisible, setProgressVisible] = useState(false);
 
   useEffect(() => {
-    const socket = io("https://lexart-fs-software-test.vercel.app", {
+    const socket = io("https://lexart-fs-software-test-backend.onrender.com", {
       transports: ["websocket"],
     });
     socket.on("deleteProgress", ({ progress }) => {
