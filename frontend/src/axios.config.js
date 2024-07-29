f59beb058e4fd614fe46ade18a56fb2f79d5b1be
Augustofrx <augustoiphar99@hotmail.com>
2024-07-28 21:08:27 -0300

    fix: api url

--- frontend/src/axios.config.js
+++ frontend/src/axios.config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://lexart-fs-software-test-backend.onrender.com",
+  baseURL: "https://lexart-fs-software-test-backend.onrender.com/api",
 });
 
 instance.interceptors.request.use(
